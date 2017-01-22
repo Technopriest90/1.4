617b7ebb6f7ab6394f14a3c6281053bb59d7a68c
Technopriest90 <erokhin.ag@yandex.ru>
2017-01-22 19:26:22 +0300

    423

---
+++ 1.4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+     //  class c4_1 // Продемонстрировать работу оператора %
+     //   {
+     //       static void Main()
+     //       {
+     //       int iresult, irem;
+     //       double dresult, drem;
+     //
+     //       iresult = 10 / 3;
+     //       irem = 10 % 3;
+     //
+     //       dresult = 10.0 / 3.0;
+     //       drem = 10.0 % 3.0;
+     //
+     //       Console.WriteLine("Результат и остаток от деления 10/3: " + iresult + " " + irem);
+     //       Console.WriteLine("Результат и остаток от деления 10.0 / 3.0: " + dresult + " " + drem);
+     //       Console.ReadKey();
+     //
+     //
+     //       }
+     //   }
+
+   // class c4_2  // декремент и инкремент
+   // {
+   // static void Main()
+   // {
+   //     int x, y;
+   //     int i;
+   //
+   //     x = 1;
+   //     y = 0;
+   //     Console.WriteLine("Ряд чисел, полученных " + "с помощью оператора y=y+x++");
+   //     for(i=0;i<10;i++)
+   //     {
+   //         y = y + x++; //постфиксная форма оператора ++
+   //
+   //         Console.WriteLine(y + " ");
+   //     }
+   //     Console.WriteLine();
+   //
+   //     x = 1;
+   //     y = 0;
+   //     Console.WriteLine("Ряд чисел, полученных " + "с помощью оператора y=y+ ++x;");
+   //     for(i=0;i<10; i++)
+   //     {
+   //         y = y + ++x; //префиксная форма оператора ++
+   //
+   //         Console.WriteLine(y + "");
+   //     }
+   //     Console.WriteLine();
+   //     Console.ReadKey();
+   // }
+   // }
+    123
